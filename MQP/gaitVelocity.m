d7587d44efe0765947857fd746e4e00a0fdc5916
Danielle LaRose <dmlarose93@gmail.com>
2015-02-17 09:42:33 -0500

    edit comments

--- MQP/gaitVelocity.m
+++ MQP/gaitVelocity.m
@@ -1,6 +1,8 @@
 %Calculates the average gait velocity
 %c is the average steps/second
+%   (from averageCadence)
 %s is the average steps/meter
+%   (from averageStepLength)
 function gv = gaitVelocity(c,s)
 
 gv = c/s;
